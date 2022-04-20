92ff3caa9b67a868f6ec079ac2489b2874b99cf5
ldr1112 <95614977+ldr1112@users.noreply.github.com>
2022-04-20 14:50:53 +0900

    2 commit

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
-const title = document.querySelector("div.hello:first-child h1")
+const h1 = document.querySelector("div.hello:first-child h1")
 
 //클릭 이벤트 (함수를 사용하여 클릭으로 이벤트를 발생하게 할 수 있음)
 function handTitleClick() {
-  title.style.color = "red"
+  const currentColor = h1.style.color
+  let newColor
+  if(currentColor === "blue") {
+    newColor = "tomato"
+  } else {
+    newColor = "blue"
+  }
+  h1.style.color = newColor
 }
 
-title.addEventListener("click", handTitleClick)
+h1.addEventListener("click", handTitleClick)
+
