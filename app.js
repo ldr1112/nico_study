6a8d363b178117305939346c51e974050f3037a0
ldr1112 <95614977+ldr1112@users.noreply.github.com>
2022-04-25 16:51:17 +0900

    toggle

--- app.js
+++ app.js
@@ -1,13 +1,8 @@
 const h1 = document.querySelector("div.hello:first-child h1")
 
-// 클릭 이벤트 (함수를 사용하여 클릭으로 이벤트를 발생하게 할 수 있음)
+// toggle은 한번 한번 클릭으로 함수를 받아드림(버튼이랑 비슷함), 코드도 간편함
 function handTitleClick() {
-  const clicked = "active"
-  if(h1.className === clicked) {
-    h1.className = ""
-  } else {
-    h1.className = clicked
-  }
+  h1.classList.toggle("clicked")
 }
 
 h1.addEventListener("click", handTitleClick)
