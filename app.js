89724e830a653a9170c76a433cc9061ee498200e
ldr1112 <95614977+ldr1112@users.noreply.github.com>
2022-04-25 13:51:57 +0900

    3 commit

--- app.js
+++ app.js
@@ -1,16 +1,13 @@
 const h1 = document.querySelector("div.hello:first-child h1")
 
-//클릭 이벤트 (함수를 사용하여 클릭으로 이벤트를 발생하게 할 수 있음)
+// 클릭 이벤트 (함수를 사용하여 클릭으로 이벤트를 발생하게 할 수 있음)
 function handTitleClick() {
-  const currentColor = h1.style.color
-  let newColor
-  if(currentColor === "blue") {
-    newColor = "tomato"
+  const clicked = "active"
+  if(h1.className === clicked) {
+    h1.className = ""
   } else {
-    newColor = "blue"
+    h1.className = clicked
   }
-  h1.style.color = newColor
 }
 
 h1.addEventListener("click", handTitleClick)
-
